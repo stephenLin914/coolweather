e23822ebf2ef8c8a3754991e304cde714998558e
linzh <linzhihui914@163.com>
2019-05-31 09:37:35 +0800

    First commit

---
+++ app/src/androidTest/java/com/example/coolweather/ExampleInstrumentedTest.java
@@ -0,0 +1,77 @@
+package com.example.coolweather;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("com.example.coolweather", appContext.getPackageName());
+        //参数字符串，如果拼接在请求链接之后，需要对中文进行 URLEncode   字符集 UTF-8
+        String param = "key=a3fbc4bbccbb47ea893b0b557f86f1e7&location=北京";
+        StringBuilder sb = new StringBuilder();
+        InputStream is = null;
+        BufferedReader br = null;
+        PrintWriter out = null;
+        try {
+            //接口地址
+            String url = "https://api.heweather.net/s6/weather";
+            URL uri = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) uri.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setReadTimeout(5000);
+            connection.setConnectTimeout(10000);
+            connection.setRequestProperty("accept", "*/*");
+            //发送参数
+            connection.setDoOutput(true);
+            out = new PrintWriter(connection.getOutputStream());
+            out.print(param);
+            out.flush();
+            //接收结果
+            is = connection.getInputStream();
+            br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line;
+            //缓冲逐行读取
+            while ( ( line = br.readLine() ) != null ) {
+                sb.append(line);
+            }
+            System.out.println(sb.toString());
+        } catch ( Exception ignored ) {
+        } finally {
+            //关闭流
+            try {
+                if(is!=null){
+                    is.close();
+                }
+                if(br!=null){
+                    br.close();
+                }
+                if (out!=null){
+                    out.close();
+                }
+            } catch ( Exception ignored ) {}
+        }
+    }
+}
